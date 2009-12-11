781018c1da38c08fd08a22227d6bbd8780c052bd
Harald Servat <harald.servat@bsc.es>
2009-12-11 10:04:47 +0000

    Fix parameter types

--- folding/service-kriger/kriger_wrapper.c
+++ folding/service-kriger/kriger_wrapper.c
@@ -9,7 +9,7 @@ void Kriger_Region (int num_in_samples, double *X_samples, double *Y_samples, in
 	F77(call_kriger_region) (&num_in_samples, X_samples, Y_samples, &num_out_samples, Y_out_samples, &min_value, &max_value);
 }
 
-void Kriger_Point (int num_in_samples, double *X_samples, double *Y_samples, double *X_in, double *Y_out, double min_value, double max_value)
+void Kriger_Point (int num_in_samples, double *X_samples, double *Y_samples, double X_in, double *Y_out, double min_value, double max_value)
 {
-	F77(call_kriger_point) (&num_in_samples, X_samples, Y_samples, X_in, Y_out, &min_value, &max_value);
+	F77(call_kriger_point) (&num_in_samples, X_samples, Y_samples, &X_in, Y_out, &min_value, &max_value);
 }
