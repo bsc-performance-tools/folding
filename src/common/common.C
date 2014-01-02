241d09907aaf26c57180afa6b13728f4e2845489
Harald <harald@__.es>
2014-01-02 14:47:43 +0000

    Reordering files

--- src/common/common.C
+++ src/common/common.C
@@ -37,9 +37,8 @@ static char __attribute__ ((unused)) rcsid[] = "$Id$";
 #include <iomanip>
 #include <iostream>
 #include <fstream>
-#include <algorithm>
 #include <sys/stat.h>
-
+#include <algorithm>
 #include <sys/types.h>
 #include <dirent.h>
 #include <stdio.h>
@@ -48,9 +47,6 @@ static char __attribute__ ((unused)) rcsid[] = "$Id$";
 #include <time.h>
 #include <stdlib.h>
 #include <string.h>
-
-#define PAPI_MIN_COUNTER   42000000
-#define PAPI_MAX_COUNTER   42999999
 
 string common::convertDouble (double d, int i)
 {
@@ -127,114 +123,6 @@ bool common::existsDir (string dir)
 	struct stat buffer;
 	stat(dir.c_str(), &buffer);
 	return (buffer.st_mode & S_IFMT) == S_IFDIR;
-}
-
-unsigned common::lookForCounter (string &name, UIParaverTraceConfig *pcf)
-{
-	vector<unsigned> vtypes = pcf->getEventTypes();
-
-	for (unsigned u = 0; u < vtypes.size(); u++)
-	{
-		if (vtypes[u] >= PAPI_MIN_COUNTER && vtypes[u] <= PAPI_MAX_COUNTER)
-		{
-			string s = pcf->getEventType (vtypes[u]);
-			if (s.length() > 0)
-			{
-				string tmp = s.substr (s.find ('(')+1, s.find (')', s.find ('(')+1) - (s.find ('(') + 1));
-				if (tmp == name)
-					return vtypes[u];
-			}
-		}
-	}
-
-	return 0;
-}
-
-unsigned common::lookForValueString (UIParaverTraceConfig *pcf, unsigned type, string str, bool &found)
-{
-	found = false;
-
-	vector<unsigned> vtypes = pcf->getEventTypes();
-	if (find (vtypes.begin(), vtypes.end(), type) != vtypes.end())
-	{
-		vector<unsigned> v;
-		try
-		{
-			v = pcf->getEventValues(type);
-		}
-		catch (...)
-		{
-			found = false;
-			return 0;
-		}
-		for (unsigned i = 0; i < v.size(); i++)
-		{
-			string evstr = pcf->getEventValue (type, v[i]);
-			if (str == evstr)
-			{
-				found = true;
-				return v[i];
-			}
-			else if (str == removeSpaces (evstr))
-			{
-				found = true;
-				return v[i];
-			}
-		}
-	}
-
-	return 0;
-}
-
-void common::lookForCallerLineInfo (UIParaverTraceConfig *pcf, unsigned id,
-	string &file, unsigned &line)
-{
-	string cl = pcf->getEventValue (30000100, id);
-	line = atoi ((cl.substr (0, cl.find (" "))).c_str());
-	int pos_open = cl.find ("(");
-	int pos_close = cl.find (")");
-	file = cl.substr (pos_open+1, pos_close-pos_open-1);
-}
-
-void common::lookForCallerASTInfo (UIParaverTraceConfig *pcf, unsigned caller,
-	unsigned callerlineast, string &routine, string &file, unsigned &bline,
-	unsigned &eline)
-{
-	string cl = pcf->getEventValue (30000200, callerlineast);
-
-	bline = 0;
-	eline = 0;
-
-	/* Check whether we have a line separator or not */
-
-	int sep_position, par_position = cl.find ("(");
-	string tmp = cl.substr (0, par_position);
-	if ((sep_position = tmp.find ("-")) != string::npos)
-	{
-		string tmp1 = tmp.substr (0, sep_position);
-		string tmp2 = tmp.substr (sep_position+1);
-		bline = atoi (tmp1.c_str());
-		eline = atoi (tmp2.c_str());
-	}
-	else
-	{
-		/* If can't find -, then there isn't a separator */
-		bline = eline = atoi (tmp.c_str());
-	}
-
-	int pos_open = par_position;
-	int pos_close = cl.find (")");
-	file = cl.substr (pos_open+1, pos_close-pos_open-1);
-	routine = pcf->getEventValue (30000000, caller);
-}
-
-void common::lookForCallerFullInfo (UIParaverTraceConfig *pcf, unsigned caller,
-	  unsigned callerline, unsigned callerlineast, string &routine, string &file,
-	  unsigned &line, unsigned &bline, unsigned &eline)
-{
-	lookForCallerASTInfo (pcf, caller, callerlineast, routine, file, bline,
-	  eline);
-	lookForCallerLineInfo (pcf, callerline, file, line);
 }
 
 void common::CleanMetricsDirectory_r (char *dir)
