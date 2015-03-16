38f3f9d3aa2fe772bcb9af8c61b0dfa3dfca2ab9
Harald <harald@__.es>
2015-03-16 14:56:24 +0000

    Documentation fixes

---
+++ doc/user-guide/images.pl
@@ -0,0 +1,188 @@
+# LaTeX2HTML 2008 (1.71)
+# Associate images original text with physical files.
+
+
+$key = q/{lstlisting}[float=h,language=XML,frame=tb,basicstyle={small{{ttfamily{,keywordsDP,RESOURCE_STALLS,RESOURCE_STALLS:RS<slashset><slashcpu>{lstlisting};LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="796" HEIGHT="320" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img6.png"
+ ALT="\begin{lstlisting}[
+float=h,
+language=XML,
+frame=tb,
+basicstyle=\small\ttfam...
+...PAPI_VEC_DP,
+RESOURCE_STALLS,RESOURCE_STALLS:RS
+&lt;/set&gt;
+&lt;/cpu&gt;
+\end{lstlisting}">|; 
+
+$key = q/{lstlisting}[float=h,language=XML,frame=tb,basicstyle={small{{ttfamily{,keywordsEJECT,PM_LD_MISS_L1,PM_LD_REF_L1<slashset><slashcpu>{par{{lstlisting};LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="807" HEIGHT="569" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img9.png"
+ ALT="\begin{lstlisting}[
+float=h,
+language=XML,
+frame=tb,
+basicstyle=\small\ttfam...
+...PLU_STALL_REJECT,PM_LD_MISS_L1,PM_LD_REF_L1
+&lt;/set&gt;
+&lt;/cpu&gt;
+\par
+\end{lstlisting}">|; 
+
+$key = q/{lstlisting}[float=h,language=XML,frame=tb,basicstyle={small{{ttfamily{,keywordsP,PAPI_VEC_DP,PAPI_FP_INS,PAPI_BR_MSP<slashset><slashcpu>{lstlisting};LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="797" HEIGHT="263" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img10.png"
+ ALT="\begin{lstlisting}[
+float=h,
+language=XML,
+frame=tb,
+basicstyle=\small\ttfam...
+...,PAPI_VEC_SP,PAPI_VEC_DP,PAPI_FP_INS,PAPI_BR_MSP
+&lt;/set&gt;
+&lt;/cpu&gt;
+\end{lstlisting}">|; 
+
+$key = q/{lstlisting}[float=h,language=XML,frame=tb,basicstyle={small{{ttfamily{,keywords,PM_RUN_CYC,PM_CMPLU_STALL_DMISS_L2L3<slashset><slashcpu>{lstlisting};LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="828" HEIGHT="473" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img7.png"
+ ALT="\begin{lstlisting}[
+float=h,
+language=XML,
+frame=tb,
+basicstyle=\small\ttfam...
+...N_INST_CMPL,PM_RUN_CYC,PM_CMPLU_STALL_DMISS_L2L3
+&lt;/set&gt;
+&lt;/cpu&gt;
+\end{lstlisting}">|; 
+
+$key = q/{lstlisting}[frame=tb,basicstyle={small{{ttfamily{,keywordstyle={small{{bfseries00000S400030002PAPI_TOT_CYC4000PAPI_TOT_INS20002012313450{lstlisting};LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="713" HEIGHT="110" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img12.png"
+ ALT="\begin{lstlisting}[
+frame=tb,
+basicstyle=\small\ttfamily ,
+keywordstyle=\smal...
+... 3000 2 PAPI_TOT_CYC 4000 PAPI_TOT_INS 2000 2 0 1 2 3 1 3 4 5 0
+\end{lstlisting}">|; 
+
+$key = q/{lstlisting}[float=h,language=XML,frame=tb,basicstyle={small{{ttfamily{,keywordsed="yes"type="default"period="50m"variability="10m"slash>{lstlisting};LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="732" HEIGHT="51" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img2.png"
+ ALT="\begin{lstlisting}[
+float=h,
+language=XML,
+frame=tb,
+basicstyle=\small\ttfam...
+...d=''yes'' type=''default'' period=''50m'' variability=''10m''/&gt;
+\end{lstlisting}">|; 
+
+$key = q/50{pm}10;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="53" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img3.png"
+ ALT="$50{\pm}10$">|; 
+
+$key = q/{lstlisting}[float=h,language=XML,frame=tb,basicstyle={small{{ttfamily{,keywordsALLS:ROB_FULL,RESOURCE_STALLS:RS_FULL<slashset><slashcpu>{lstlisting};LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="827" HEIGHT="397" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img5.png"
+ ALT="\begin{lstlisting}[
+float=h,
+language=XML,
+frame=tb,
+basicstyle=\small\ttfam...
+...RESOURCE_STALLS:ROB_FULL,RESOURCE_STALLS:RS_FULL
+&lt;/set&gt;
+&lt;/cpu&gt;
+\end{lstlisting}">|; 
+
+$key = q/N;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="20" HEIGHT="15" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img13.png"
+ ALT="$N$">|; 
+
+$key = q/{lstlisting}[float=h,language=XML,frame=tb,basicstyle={small{{ttfamily{,keywordsx><samplingenabled="yes">1-5<slashsampling><slashcallers>{lstlisting};LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="578" HEIGHT="127" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img11.png"
+ ALT="\begin{lstlisting}[
+float=h,
+language=XML,
+frame=tb,
+basicstyle=\small\ttfam...
+...1-3&lt;/pacx&gt;
+&lt;sampling enabled=''yes''&gt;1-5&lt;/sampling&gt;
+&lt;/callers&gt;
+\end{lstlisting}">|; 
+
+$key = q/{lstlisting}[float=h,language=XML,frame=tb,basicstyle={small{{ttfamily{,keywordsYC,RESOURCE_STALLS,RESOURCE_STALLS:RS<slashset><slashcpu>{lstlisting};LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="827" HEIGHT="378" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img4.png"
+ ALT="\begin{lstlisting}[
+float=h,
+language=XML,
+frame=tb,
+basicstyle=\small\ttfam...
+...,PAPI_TOT_CYC,RESOURCE_STALLS,RESOURCE_STALLS:RS
+&lt;/set&gt;
+&lt;/cpu&gt;
+\end{lstlisting}">|; 
+
+$key = q/{lstlisting}[float=h,language=XML,frame=tb,basicstyle={small{{ttfamily{,keywordsPAPI_TOT_INS<slashvalue><slashcomponent>{par{<slashmodel>{lstlisting};LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="689" HEIGHT="549" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img14.png"
+ ALT="\begin{lstlisting}[
+float=h,
+language=XML,
+frame=tb,
+basicstyle=\small\ttfam...
+...k''&gt;
+&lt;value&gt; PAPI_TOT_INS &lt;/value&gt;
+&lt;/component&gt;
+\par
+&lt;/model&gt;
+\end{lstlisting}">|; 
+
+$key = q/{lstlisting}[language=C++,frame=tb,basicstyle={footnotesize{{ttfamily{,keywordst,startRegion,durationRegion,vs);f.close();}{par{return0;}{lstlisting};LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="840" HEIGHT="1642" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img15.png"
+ ALT="\begin{lstlisting}[
+language=C++,
+frame=tb,
+basicstyle=\footnotesize\ttfamily...
+...rtRegion,
+durationRegion, vs);
+f.close();
+}
+\par
+return 0;
+}
+\end{lstlisting}">|; 
+
+$key = q/{lstlisting}[float=h,language=XML,frame=tb,basicstyle={small{{ttfamily{,keywordsCMPL,PM_RUN_CYC,PM_CMPLU_STALL_VECTOR<slashset><slashcpu>{lstlisting};LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="767" HEIGHT="473" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img8.png"
+ ALT="\begin{lstlisting}[
+float=h,
+language=XML,
+frame=tb,
+basicstyle=\small\ttfam...
+...M_RUN_INST_CMPL,PM_RUN_CYC,PM_CMPLU_STALL_VECTOR
+&lt;/set&gt;
+&lt;/cpu&gt;
+\end{lstlisting}">|; 
+
+1;
+
---
+++ doc/user-guide/internals.pl
@@ -0,0 +1,90 @@
+# LaTeX2HTML 2008 (1.71)
+# Associate internals original text with physical files.
+
+
+$key = q/fig:444_namd_instructions/;
+$ref_files{$key} = "$dir".q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cha:GetATrace/;
+$ref_files{$key} = "$dir".q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:CollectingAppropriatePerformanceCounters/;
+$ref_files{$key} = "$dir".q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:tool_design_interpolate_instances/;
+$ref_files{$key} = "$dir".q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:tool_design_interpolate_model/;
+$ref_files{$key} = "$dir".q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:444_namd_instructions_paraver/;
+$ref_files{$key} = "$dir".q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:tool_design_interpolate_interpolation/;
+$ref_files{$key} = "$dir".q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:EnableSamplingMechanism/;
+$ref_files{$key} = "$dir".q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:contents_folding_package/;
+$ref_files{$key} = "$dir".q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/subsec:ContentsOfInstallation/;
+$ref_files{$key} = "$dir".q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:dataflow_1st_component_folding/;
+$ref_files{$key} = "$dir".q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:tool_design_interpolate_sample_selector/;
+$ref_files{$key} = "$dir".q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:Nemo_cluster1_instructionmix/;
+$ref_files{$key} = "$dir".q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/subsubsec:ManualExample/;
+$ref_files{$key} = "$dir".q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:tool_design_interpolate_callstackprocessor/;
+$ref_files{$key} = "$dir".q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/subsec:LibtoolsInstallation/;
+$ref_files{$key} = "$dir".q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:Nemo_cluster1_callers_paraver/;
+$ref_files{$key} = "$dir".q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/subsubsec:ClusteredExample/;
+$ref_files{$key} = "$dir".q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cha:UserGuide/;
+$ref_files{$key} = "$dir".q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:444_namd_ratio_per_instruction/;
+$ref_files{$key} = "$dir".q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:tool_design_interpolate_instance_separator/;
+$ref_files{$key} = "$dir".q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
