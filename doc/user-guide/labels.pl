509b29269459ad9bcc9dc17b9eed3e1e666265ed
Harald Servat <harald.servat@bsc.es>
2015-03-16 14:56:24 +0000

    Documentation fixes

---
+++ doc/user-guide/labels.pl
@@ -0,0 +1,229 @@
+# LaTeX2HTML 2008 (1.71)
+# Associate labels original text with physical files.
+
+
+$key = q/fig:444_namd_instructions/;
+$external_labels{$key} = "$URL/" . q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cha:GetATrace/;
+$external_labels{$key} = "$URL/" . q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:CollectingAppropriatePerformanceCounters/;
+$external_labels{$key} = "$URL/" . q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:tool_design_interpolate_instances/;
+$external_labels{$key} = "$URL/" . q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:tool_design_interpolate_model/;
+$external_labels{$key} = "$URL/" . q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:444_namd_instructions_paraver/;
+$external_labels{$key} = "$URL/" . q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:tool_design_interpolate_interpolation/;
+$external_labels{$key} = "$URL/" . q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:EnableSamplingMechanism/;
+$external_labels{$key} = "$URL/" . q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:contents_folding_package/;
+$external_labels{$key} = "$URL/" . q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/subsec:ContentsOfInstallation/;
+$external_labels{$key} = "$URL/" . q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:dataflow_1st_component_folding/;
+$external_labels{$key} = "$URL/" . q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:tool_design_interpolate_sample_selector/;
+$external_labels{$key} = "$URL/" . q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:Nemo_cluster1_instructionmix/;
+$external_labels{$key} = "$URL/" . q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/subsubsec:ManualExample/;
+$external_labels{$key} = "$URL/" . q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:tool_design_interpolate_callstackprocessor/;
+$external_labels{$key} = "$URL/" . q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/subsec:LibtoolsInstallation/;
+$external_labels{$key} = "$URL/" . q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:Nemo_cluster1_callers_paraver/;
+$external_labels{$key} = "$URL/" . q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/subsubsec:ClusteredExample/;
+$external_labels{$key} = "$URL/" . q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cha:UserGuide/;
+$external_labels{$key} = "$URL/" . q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:444_namd_ratio_per_instruction/;
+$external_labels{$key} = "$URL/" . q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:tool_design_interpolate_instance_separator/;
+$external_labels{$key} = "$URL/" . q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
+
+# LaTeX2HTML 2008 (1.71)
+# labels from external_latex_labels array.
+
+
+$key = q/fig:444_namd_instructions/;
+$external_latex_labels{$key} = q|1.1|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:CollectingAppropriatePerformanceCounters/;
+$external_latex_labels{$key} = q|2.2|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:tool_design_interpolate_instances/;
+$external_latex_labels{$key} = q|3.2|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:tool_design_interpolate_model/;
+$external_latex_labels{$key} = q|3.6|; 
+$noresave{$key} = "$nosave";
+
+$key = q/lst:Extrae_counters_ibm_power7/;
+$external_latex_labels{$key} = q|2.6|; 
+$noresave{$key} = "$nosave";
+
+$key = q/lst:Extrae_counters_basic/;
+$external_latex_labels{$key} = q|2.8|; 
+$noresave{$key} = "$nosave";
+
+$key = q/lst:Extrae_counters_ibm_power5/;
+$external_latex_labels{$key} = q|2.7|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:tool_design_interpolate_interpolation/;
+$external_latex_labels{$key} = q|3.5|; 
+$noresave{$key} = "$nosave";
+
+$key = q/lst:Extrae_counters_intel_haswell/;
+$external_latex_labels{$key} = q|2.2|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:contents_folding_package/;
+$external_latex_labels{$key} = q|1.1|; 
+$noresave{$key} = "$nosave";
+
+$key = q/subsec:ContentsOfInstallation/;
+$external_latex_labels{$key} = q|1.1.2|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:dataflow_1st_component_folding/;
+$external_latex_labels{$key} = q|3.1|; 
+$noresave{$key} = "$nosave";
+
+$key = q/lst:ExampleGenerationInterpolation/;
+$external_latex_labels{$key} = q|4.1|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:tool_design_interpolate_instance_separator/;
+$external_latex_labels{$key} = q|3.3|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cha:GetATrace/;
+$external_latex_labels{$key} = q|2|; 
+$noresave{$key} = "$nosave";
+
+$key = q/lst:ExampleGenerationInterpolationOutput/;
+$external_latex_labels{$key} = q|3.1|; 
+$noresave{$key} = "$nosave";
+
+$key = q/lst:Extrae_enable_sampling_basic/;
+$external_latex_labels{$key} = q|2.1|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:444_namd_instructions_paraver/;
+$external_latex_labels{$key} = q|1.4|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:EnableSamplingMechanism/;
+$external_latex_labels{$key} = q|2.1|; 
+$noresave{$key} = "$nosave";
+
+$key = q/lst:Extrae_counters_intel_nehalem/;
+$external_latex_labels{$key} = q|2.4|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:tool_design_interpolate_sample_selector/;
+$external_latex_labels{$key} = q|3.4|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:Nemo_cluster1_instructionmix/;
+$external_latex_labels{$key} = q|1.3|; 
+$noresave{$key} = "$nosave";
+
+$key = q/subsubsec:ManualExample/;
+$external_latex_labels{$key} = q|1.1.3|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:tool_design_interpolate_callstackprocessor/;
+$external_latex_labels{$key} = q|3.7|; 
+$noresave{$key} = "$nosave";
+
+$key = q/subsec:LibtoolsInstallation/;
+$external_latex_labels{$key} = q|1.2.1|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:Nemo_cluster1_callers_paraver/;
+$external_latex_labels{$key} = q|1.5|; 
+$noresave{$key} = "$nosave";
+
+$key = q/subsubsec:ClusteredExample/;
+$external_latex_labels{$key} = q|1.1.3|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cha:UserGuide/;
+$external_latex_labels{$key} = q|1|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:444_namd_ratio_per_instruction/;
+$external_latex_labels{$key} = q|1.2|; 
+$noresave{$key} = "$nosave";
+
+$key = q/lst:Extrae_counters_intel_sandybridge/;
+$external_latex_labels{$key} = q|2.3|; 
+$noresave{$key} = "$nosave";
+
+$key = q/lst:Extrae_enable_sampling_callers/;
+$external_latex_labels{$key} = q|2.9|; 
+$noresave{$key} = "$nosave";
+
+$key = q/lst:Folding_sample_model/;
+$external_latex_labels{$key} = q|3.2|; 
+$noresave{$key} = "$nosave";
+
+$key = q/lst:Extrae_counters_ibm_power8/;
+$external_latex_labels{$key} = q|2.5|; 
+$noresave{$key} = "$nosave";
+
+1;
+
